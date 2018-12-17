84e647ce507f9a1538f323f42e289f735bc65532
Yudi Andela <yudhi.andhela@gmail.com>
2018-12-17 09:00:14 +0700

    Merapikan halaman detail

--- resources/views/anggota/show.blade.php
+++ resources/views/anggota/show.blade.php
@@ -2,15 +2,31 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
+        <div class="row">
+            <div class="col-md-4">
                 <div class="card">
-                    <div class="card-header">
-                        Detail {{ $anggota->name }}
+                    <div class="card-body text-center">
+                        <img src="/storage/avatar3.png" alt="" class="rounded-circle border">
+                        <div class="mt-2">
+                            <h4>{{ $anggota->name }}</h4>
+                            <p>{{ $anggota->pangkat->short . ' NRP ' . $anggota->nrp }} </p>
+                        </div>
+                        <div class="mt-2">
+                            <a href="#" class="btn btn-primary btn-sm">Follow</a>
+                            <a href="#" class="btn btn-danger btn-sm">Message</a>
+                        </div>
                     </div>
+                    <div class="list-group list-group-flush">
+                        <a href="#" class="list-group-item list-group-item-action">Cras justo odio</a>
+                        <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+                        <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="card">
                     <div class="card-body">
-                        Nama : {{ $anggota->name }}<br>
-                        Pangkat : {{ $anggota->pangkat->name }}
+                        Kontent disini
                     </div>
                 </div>
             </div>
